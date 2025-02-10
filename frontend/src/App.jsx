c29f49a12f5968d60e0293d31b9825826e43e45d
Achiever Umunwa <chukwummnl@gmail.com>
2025-02-10 19:13:47 +0100

    route catch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import UserProfile from "./components/users/UserProfile";
 import TicketForm from "./components/forms/TicketForm";
 import Tickets from "./components/tickets/Tickets";
 import Login from "./components/forms/LoginForm";
-import reactLogo from "/react.svg";
+// import reactLogo from "/react.svg";
 import VerifyEmailPage from "./components/users/VerifyEmailPage";
 import AllUsers from "./components/users/AllUsers";
 
@@ -14,55 +14,47 @@ function App() {
   const userId = localStorage.getItem("userId");
 
   return (
-    <>
-      <div className="min-h-screen items-center justify-center bg-gray-300">
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<Navigate to="/" />} />
+    <div className="min-h-screen items-center justify-center bg-gray-300">
+      <BrowserRouter>
+        <Routes>
+          {/* Main routes */}
+          <Route path="/users" element={<AllUsers />} />
+          <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
+          <Route
+            path="/"
+            element={
+              userId ? <Tickets userId={userId} /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/:id"
+            element={
+              userId ? <Tickets userId={userId} /> : <Navigate to="/login" />
+            }
+          />
+          <Route path="/register" element={<ClientRegisterForm />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/aareg" element={<AgentAdminRegisterForm />} />
+          <Route
+            path="/profile/:id?"
+            element={
+              userId ? (
+                <UserProfile userId={userId} />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/create"
+            element={userId ? <TicketForm /> : <Navigate to="/login" />}
+          />
 
-            <Route path="/users" element={<AllUsers />} />
-
-            <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
-
-            <Route
-              path="/"
-              element={
-                userId ? <Tickets userId={userId} /> : <Navigate to="/login" />
-              }
-            />
-
-            <Route
-              path="/:id"
-              element={
-                userId ? <Tickets userId={userId} /> : <Navigate to="/login" />
-              }
-            />
-
-            <Route path="/register" element={<ClientRegisterForm />} />
-
-            <Route path="/login" element={<Login />} />
-
-            <Route path="/aareg" element={<AgentAdminRegisterForm />} />
-
-            <Route
-              path="/profile/:id?"
-              element={
-                userId ? (
-                  <UserProfile userId={userId} />
-                ) : (
-                  <Navigate to="/login" />
-                )
-              }
-            />
-
-            <Route
-              path="/create"
-              element={userId ? <TicketForm /> : <Navigate to="/login" />}
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
+          {/* Catch-all route should be LAST */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
